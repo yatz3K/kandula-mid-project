dd70a20e567d0ab86fa460c1a3f85566538eee4d
Itzick Sabi <84976814+yatz3K@users.noreply.github.com>
2022-04-19 09:08:37 +0300

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,7 +0,0 @@
-module "kandula_vpc" {
-  source = "./vpc_module"
-  vpc_cidr_block = "192.168.0.0/16"
-  private_subnet_list = ["192.168.10.0/24", "192.168.20.0/24"]
-  public_subnet_list = ["192.168.100.0/24", "192.168.200.0/24"]
-  aws_availability_zones = slice(data.aws_availability_zones.available.*.names[0], 0, 2)
-}
